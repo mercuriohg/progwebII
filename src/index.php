461fad6d186f4667d2b1223df4b6bc9cfcdd0061
mercuriohg <gomesarthursevero6@gmail.com>
2026-03-16 09:31:15 -0300

    Mais exercícios de php

--- src/index.php
+++ src/index.php
@@ -44,7 +44,7 @@
     }
     // Verificador de data
     $lista = ["", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
-    $dia = 40;
+    $dia = 30;
     $mes = 8;
     $ano = 2023;
     
@@ -53,4 +53,88 @@
     } else {
             echo "<br> Dia ou mês inválido.";
     }    
+
+    // Exercício 1.
+    $horas = 8;
+    $valor_hora = 50;
+    $valor = $horas * 5 * $valor_hora * 4;
+    echo "<br> o valor recebido pelo funcionário é R$ ". $valor;
+
+    // Exercício 2
+    $num1 = 10;
+    $num2 = 10;
+    $num3 = 9;
+    $maior = 0;
+    if ($num1 > $num2 or $num1 > $num3){
+        $maior = $num1;
+        echo "<br>O maior número é " .$maior;
+    }else if ($num2 > $num1 or $num2 > $num3){
+        $maior = $num2;
+        echo "<br>O maior número é " .$maior;
+    }else{
+        $maior = $num3;
+        echo "<br>O maior número é " .$maior;
+    } 
+    //Exercício 3
+    $sexo = "m";
+    if (strtoupper($sexo) == "M"){
+         $alt = 1.8;
+         $formula = (72.7*$alt) - 58;
+         echo "<br> O peso ideal para homem é ".$formula . "Kg";
+    }
+    else{
+        $alt = 1.8;
+        $formula = (62.1*$alt) - 44.7;
+        echo "<br> O peso ideal para mulher é ".$formula ."Kg";
+    }
+    //Exercício 4
+    $nota1 = 10;
+    $nota2 = 9;
+    $nota3 = 7.5;
+    $media = 0;
+    
+    if ($nota1 > 10 or $nota2 > 10 or $nota3 > 10){
+        echo "<br>Nota inválida";
+    }
+    else if ($nota1 < 0 or $nota2 < 0 or $nota3 < 0){
+        echo "<br>Nota inválida";
+    }
+    else{
+        $media = ($num1+$num2+$num3) / 3;
+        echo "<br>Média final é ". round($media,1);
+    }
+   //Exercício 5
+   $peso_pessoa = 93;
+   $altura = 1.83;
+   $elevado = $altura*$altura;
+   $imc = $peso_pessoa / $elevado;
+   echo "<br>Seu imc é " . round($imc,2);
+   if ($imc < 18.5){
+    echo " / Magreza.";
+   } else if ($imc < 24.5){
+    echo " / Normal.";
+   }
+   else if ($imc < 29.9){
+    echo " / Sobrepeso.";
+   }
+   else if ($imc < 39.9){
+    echo " / Obesidade.";
+   }
+    else{
+        echo " / Obesidade Grave";
+    }
+ 
+   //Exercício 6
+   $chico = 0.8;
+   $juca = 0.6;
+   $ano = 0;
+   while($juca < $chico){
+    $juca = $juca + 0.09;
+    $chico = $chico + 0.06;
+    $ano++;
+    
+   }
+   echo "<br>Foram necessários ". $ano . " anos para Juca ultrapassar Chico na altura";
+   //Exercício 7
+   
 ?>
