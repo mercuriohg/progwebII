90cdb336e7d8bd56392c473ec16355cf52e0be1e
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2026-03-30 09:22:19 -0300

    Add files via upload

---
+++ ex_array.php
@@ -0,0 +1,23 @@
+<?php 
+    // Exercício 2
+    $array_num = [1, 20, 40, 50, 60, 80, 200, 80, 10, 70];
+    $maior = $array_num[0];
+    foreach($array_num as $num){
+        if ($num > $maior){
+            $maior = $num;
+        }
+    }
+    echo $maior;
+    echo "<hr>";
+
+    //Exercício 3
+    $array_frutas = ["Maçã", "Abacate", "Pêra", "Banana", "Morango"];
+    for ($i = 0; $i < count($array_frutas); $i++){
+        echo $i+1 . " - " . $array_frutas[$i] . "<br>";
+    }
+    echo "<hr>";
+
+    //Exercício 4
+    
+
+?>
