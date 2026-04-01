f10225d885c67f5e1efc6528f08e301b14760d1c
mercuriohg <gomesarthursevero6@gmail.com>
2026-04-01 08:31:11 -0300

    Fazendo o exercício de progwebII

---
+++ src/array.php
@@ -0,0 +1,27 @@
+<?php 
+    // Exercício 2
+    $array_num = [1, 20, 40, 50, 60, 80, 200, 80, 10, 70];
+    $maior = $array_num[0];
+    foreach($array_num as $num){
+        if ($num > $maior){
+            $maior = $num;
+        }
+    }
+    echo $maior;
+    echo "<hr>";
+
+    //Exercício 3
+    $array_frutas = ["Maçã", "Abacate", "Pêra", "Banana", "Morango"];
+    for ($i = 0; $i < count($array_frutas); $i++){
+        echo $i+1 . " - " . $array_frutas[$i] . "<br>";
+    }
+    echo "<hr>";
+
+    //Exercício 4
+    $array_palavras = ["Frozen", "Abecê", "Frozen", "Grêmio","Internacional", "Grêmio"];
+    foreach( $array_palavras as $quant_palavras){
+        $contagem = array_count_values($array_palavras);    
+    }
+    print_r($contagem);
+
+?>
